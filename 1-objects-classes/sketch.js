4e1540d3ed423cbf40f7a757fd282009473a743a
JayBrMason <68661207+JayBrMason@users.noreply.github.com>
2020-09-17 12:12:31 -0700

    Update sketch.js
    Added to object 1

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,9 +1,21 @@
 //create a variable to hold one ball
+let b;
+let anotherBall;
+let thirdBall;
+let human1;
+let human2;
+
+
+
 
 
 function setup() {
   createCanvas(800, 400);
-
+  b = new CircularThing(0, 100,"red");
+  anotherBall = new CircularThing(200,20,"green");
+  thirdBall = new CircularThing(20, 20, "orange");
+  let human1 = new Person(255, 255, 255);
+  let human2 = new person(0, 0, 0);
 }
 
 
@@ -13,5 +25,42 @@ function draw(){
 
 }
 
+
+class CircularThing{
+
+  constructor(x,y,color){
+		this.x = x;
+    		this.y = y;
+           this.color = color;
+	  }
+  drawBall(){
+    stroke(0);
+    fill(this.color);
+    ellipse(this.x,this.y,10,10);
+	}
+
+  moveBall(){
+    this.x = this.x+2;
+    this.y = this.y+.5;
+  }
+}
+
+
+class Person{
+  constructor(x,y,color){
+		this.x = x;
+    		this.y = y;
+           this.color = color;
+	  }
+  drawBall(){
+    stroke(0);
+    fill(this.color);
+    ellipse(this.x,this.y,10,10);
+	}
+
+  moveBall(){
+    this.x = this.x+2;
+    this.y = this.y+.5;
+  }
 
 //ball class from which to create new balls with similar properties.
