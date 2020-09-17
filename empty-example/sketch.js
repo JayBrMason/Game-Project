7b119093a72ba2915b3d808f74d7c35f97071f3e
JayBrMason <68661207+JayBrMason@users.noreply.github.com>
2020-09-17 09:45:26 -0700

    File changed
    Deleted answers, made background orange

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -3,17 +3,6 @@ function setup() {
 }
 
 function draw(){
-	background(220,100,3);
-  fill("black")
-  ellipse(100,100,40,50);
-  fill("blue")
-  rect(200,200,100,150)
-  rect(400,300,100,100);
-  fill("green");
-  rect(375,400,150,150);
-  rect(325,410,50,100);
-  rect(500,410,50,100);
-  fill("yellow");
-  ellipse(400,340,15,15);
-  ellipse(450,340,15,15);
+  background(220, 100, 3)
+
 }
